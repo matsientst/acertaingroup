a7ad736c3b3d5261334f3c117932f572aa3c2417
matt macchia <mattmacchia@matts-MacBook-Pro.local>
2025-04-30 11:51:33 -0600

    Adding base and site

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://matsientst.github.io',
+  base: '/acertaingroup',
 
   integrations: [
     tailwind({
